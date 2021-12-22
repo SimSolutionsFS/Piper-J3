4735c69674d51df78b777144ad89ebca073cf1b7
slimit75 <limitspeed75@gmail.com>
2021-12-22 14:06:34 -0500

    Create "J3/Ground/ShowCovers"

--- plugins/J3/src/misc.cpp
+++ plugins/J3/src/misc.cpp
@@ -5,13 +5,44 @@
 
 #include "misc.h"
 
+int showCovers;
+
+int getCovers(void* inRefcon) { 
+	return showCovers; 
+}
+void setCovers(void* inRefcon, int inValue) { 
+	showCovers = inValue; 
+}
+
+XPLMDataRef drefShowTiedowns = XPLMRegisterDataAccessor("J3/Ground/ShowCovers", xplmType_Int, true, getCovers, setCovers, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+
+int toggleCovers(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, void* inRefcon) {
+	if (inPhase == 0) {
+		if (showCovers == 1) { 
+			showCovers = 0; 
+		}
+		else { 
+			showCovers = 1; 
+		}
+	}
+
+	return 1;
+}
+
+XPLMCommandRef commandCovers = XPLMCreateCommand("J3/Ground/ToggleCovers", "Toggle Covers");
+
 void miscStart() {
+	logMsg("Registering commands...");
+	XPLMRegisterCommandHandler(commandCovers, toggleCovers, 1, nullptr);
+
 	logMsg("Adding items to aircraft menu...");
 	XPLMMenuID aircraft_menu = XPLMFindAircraftMenu();
 	if (aircraft_menu) {
-		XPLMAppendMenuItemWithCommand(aircraft_menu, "Toggle Chocks/Tiedowns", nullptr);
-		XPLMAppendMenuItemWithCommand(aircraft_menu, "Toggle Tablet", nullptr);
+		XPLMAppendMenuItemWithCommand(aircraft_menu, "Toggle Covers", commandCovers);
 	}
 }
 
-void miscStop() {}
+void miscStop() {
+	logMsg("Unregistering commands...");
+	XPLMUnregisterCommandHandler(commandCovers, toggleCovers, 1, nullptr);
+}
